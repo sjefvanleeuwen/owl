d999cbb56a0f51f37c63020a48443d1fa0c8a085
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-19 00:44:13 +0100

    Add Modelhelpers

---
+++ Vs.WebVOWL.Model/ModelHelpers.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Vs.WebVOWL
+{
+    public static class ModelHelpers
+    {
+        public static void CreateSettings(ref Welcome model, long classDistance = 150, long datatypeDistance=150, double zoom = 1, bool paused = false, double translateX = 0, double translateY = 0)
+        {
+            model.Settings = new Settings
+            {
+                Global = new Global
+                {
+                    Paused = paused,
+                    Zoom = zoom.ToString(CultureInfo.InvariantCulture),
+                    Translation = new List<double>() { translateX, translateY }
+                },
+                Gravity = new Gravity
+                {
+                    ClassDistance = classDistance,
+                    DatatypeDistance = datatypeDistance
+                }
+            };
+        }
+
+        public static void CreateDataType(ref Welcome model, string id)
+        {
+            if (model.Property == null)
+                model.Property = new List<Class>();
+            model.Property.Add(
+                new Class()
+                {
+                    Id = id,
+                    Type = "owl:DatatypeProperty",
+                });
+        }
+
+        public static void CreatePropertyAttribute(ref Welcome model, string propertyReferenceId, string label, string domain, string range)
+        {
+            if (model.PropertyAttribute == null)
+                model.PropertyAttribute = new List<PropertyAttribute>();
+
+            model.PropertyAttribute.Add(
+                new PropertyAttribute()
+                {
+                    Id = propertyReferenceId,
+                    BaseIri = new Uri("https://ontologie.virtualsociety.nl/urukagina/zorgtoeslag"),
+                    Iri = new Uri("https://ontologie.virtualsociety.nl/urukagina/zorgtoeslag/drempelinkomen"),
+                    Label = new PropertyAttributeLabel() { FluffyLabel = new FluffyLabel() { IriBased = label } },
+                    Domain = domain,
+                    Range = range,
+                    Attributes = new List<string>() { "datatype" }
+                });
+        }
+
+        public static void CreateClassAttribute(ref Welcome model, string id, string label)
+        {
+            if (model.ClassAttribute == null)
+                model.ClassAttribute = new List<ClassAttribute>();
+
+            model.Class.Add(new Class() { Id = id, Type = "rdfs:Datatype" });
+            model.ClassAttribute.Add(
+                new ClassAttribute()
+                {
+                    Id = id,
+                    Iri = new Uri("http://visualdataweb.org/newOntology/zorgtoeslag"),
+                    BaseIri = new Uri("http://visualdataweb.org/newOntology/"),
+                    Label = new ClassAttributeLabel() { PurpleLabel = new PurpleLabel() { IriBased = label } },
+                    Attributes = new List<string>() { "datatype" }
+                });
+
+        }
+
+        public static void CreateClass(ref Welcome model, string id,string label, string iri="", string baseIri="")
+        {
+            if (model.Class == null)
+                model.Class = new List<Class>();
+            model.Class.Add(new Class() { Id = id, Type = "owl:Class", });
+            if (model.ClassAttribute == null)
+                model.ClassAttribute = new List<ClassAttribute>();
+            model.ClassAttribute.Add(
+                new ClassAttribute()
+                {
+                    Id = id,
+                    Label = new ClassAttributeLabel() { PurpleLabel = new PurpleLabel() { IriBased = label } },
+                });
+        }
+
+        public static void CreateHeader(ref Welcome model, string comment = "default", string title = "untitled",
+            string author = "", string version = "", string description = "", string iri = "https://ontologie.virtualsociety.nl", string baseIri = "http://www.w3.org/2000/01/rdf-schema")
+        {
+            model.Comment = comment;
+            model.Header = new Header
+            {
+                Languages = new List<string>()
+            };
+            model.Header.Languages.Add("en");
+            model.Header.Title = new Comments
+            {
+                Undefined = title
+            };
+            model.Header.Author = new List<string>() { author };
+            model.Header.Version = version;
+            model.Header.Description = new Comments
+            {
+                Undefined = description
+            };
+            model.Header.BaseIris = new List<Uri>
+            {
+                new Uri(baseIri)
+            };
+            model.Header.Iri = new Uri(iri);
+        }
+    }
+}
--- Vs.WebVOWL.Tests/OwlTests.cs
+++ Vs.WebVOWL.Tests/OwlTests.cs
@@ -12,76 +12,20 @@ namespace Vs.WebVOWL.Tests
         public void CreateClass()
         {
             Welcome model = new Welcome();
-            model.Settings = new Settings();
-            model.Settings.Global = new Global();
-            model.Settings.Global.Paused = false;
-            model.Settings.Global.Zoom = "0.68";
-            model.Settings.Global.Translation = new List<double>() { 529.3, 33.32 };
-            model.Settings.Gravity = new Gravity();
-            model.Settings.Gravity.ClassDistance = 200;
-            model.Settings.Gravity.DatatypeDistance = 200;
-            model.Settings.Global.Paused = false;
             model.Namespace = new List<object>();
-            model.Comment = "Virtual Society - Zorgtoeslag";
-            model.Header = new Header();
-            model.Header.Languages = new List<string>();
-            model.Header.Languages.Add("en");
-            model.Header.Title = new Comments();
-            model.Header.Title.Undefined = "Zorgtoeslag";
-            model.Header.Author = new List<string>() { "Sjef van Leeuwen" };
-            model.Header.Version = "0.1";
-          //  model.Header.Other = new Other();
-          //  model.Header.Other.Rights = new List<BackwardCompatibleWith>();
-          //  model.Header.Other.Rights.Add(new BackwardCompatibleWith() { Language = Language.Undefined, Identifier = "MIT License" });
+            ModelHelpers.CreateSettings(ref model);
+            ModelHelpers.CreateHeader(ref model,
+                comment: "Virtual Society - Zorgtoeslag",
+                title:"Zorgtoeslag",
+                author:"Sjef van Leeuwen",version:"0.1",description: "API configuratie voor het berekenen van zorgtoeslag");
+            ModelHelpers.CreateClass(ref model, "Zorgtoeslag","Zorgtoeslag");
+            ModelHelpers.CreateClassAttribute(ref model, "euro","Euro");
+            ModelHelpers.CreateClassAttribute(ref model, "double", "double");
+            ModelHelpers.CreateDataType(ref model, "dataTypeProperty1");
+            ModelHelpers.CreateDataType(ref model, "dataTypeProperty3");
+            ModelHelpers.CreatePropertyAttribute(ref model, "dataTypeProperty1","drempelinkomen","Zorgtoeslag", "euro");
+            ModelHelpers.CreatePropertyAttribute(ref model, "dataTypeProperty3", "woonlandfactor", "Zorgtoeslag", "double");
 
-            model.Header.Description = new Comments();
-            model.Header.Description.Undefined = "API configuratie voor het berekenen van zorgtoeslag";
-            model.Header.BaseIris = new List<Uri>();
-            model.Header.BaseIris.Add(new Uri("http://www.w3.org/2000/01/rdf-schema"));
-           /* model.Header.BaseIris.Add(new Uri("https://ontologie.virtualsociety.nl/urukagina/zorgtoeslag"));*/
-            model.Header.Iri = new  Uri("https://ontologie.virtualsociety.nl/newOntology/");
-            model.Class = new List<Class>();
-            model.Class.Add(new Class() {  Id = "Zorgtoeslag", Type = "owl:Class",  });
-            model.ClassAttribute = new List<ClassAttribute>();
-            var attributes = new List<string>();
-            attributes.Add("datatype");
-            model.Class.Add(new Class() { Id = "euro", Type = "rdfs:Datatype" });
-
-            model.ClassAttribute.Add(
-                new ClassAttribute()
-                {
-                    Id = "euro",
-                    Iri = new Uri("http://visualdataweb.org/newOntology/zorgtoeslag"),
-                    BaseIri = new Uri("http://visualdataweb.org/newOntology/"),
-                    Label = new ClassAttributeLabel() { PurpleLabel = new PurpleLabel() { IriBased = "Euro" } },
-                    Attributes = attributes
-                }) ;
-
-
-            model.ClassAttribute.Add(
-                new ClassAttribute()
-                {
-                    Id = "Zorgtoeslag",
-                    Label = new ClassAttributeLabel() { PurpleLabel = new PurpleLabel() { IriBased = "Zorgtoeslag" } },
-                });
-            model.PropertyAttribute = new List<PropertyAttribute>();
-            model.Property = new List<Class>();
-            model.Property.Add(
-                new Class() { 
-                    Id="dataTypeProperty1",
-                    Type= "owl:DatatypeProperty",
-                });
-            model.PropertyAttribute.Add(
-                new PropertyAttribute()
-                {
-                    Id = "dataTypeProperty1",
-                    BaseIri = new Uri("https://ontologie.virtualsociety.nl/urukagina/zorgtoeslag"),
-                    Iri = new Uri("https://ontologie.virtualsociety.nl/urukagina/zorgtoeslag/drempelinkomen"),
-                    Label = new PropertyAttributeLabel() { FluffyLabel = new FluffyLabel() { IriBased = "drempelinkomen" } },
-                    Domain = "Zorgtoeslag",
-                    Range = "euro",
-                    Attributes = new List<string>() { "datatype" }
-                });
 
             File.WriteAllText("./create-class.json", model.ToJson());
         }
